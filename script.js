658e50dcc4146363024bf41ef6e0d78cd66664cd
Fco Gaz <fcogaz@icloud.com>
2022-09-16 19:38:59 -0500

    // 2º commit -Branch Gaz 1 er commit dev

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // 2º commit
 let js = "amazing";
-if (js === "amazing") alert("Java Script is Fun!");
+if (js === "amazing") alert("Java Script is /watchFun!");
 
 console.log(550 + 5);
-console.log("Gaz");
+console.log("HHOLA");
+
+let firstName = "Matilda";
+let lastName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(lastName);
